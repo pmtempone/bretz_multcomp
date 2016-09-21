050ae0581dc8fd3645e273b182ac321f09c61085
pmtempone <pmtempone@gmail.com>
2016-09-21 17:34:32 -0300

    v2

--- Introduccion.R
+++ Introduccion.R
@@ -3,6 +3,7 @@
 library(ISwR)
 library(multcomp)
 
+options(scipen = 999)
 ----#Introduccion----
 
 thuesen.lm <- lm(short.velocity ~ blood.glucose,data = thuesen)
@@ -19,3 +20,13 @@ summary(thuesen.mc, test = adjusted(type = "bonferroni"))
 #The associated adjusted p-value is 0.096, twice as large as the unadjusted
 #p-value from the previous analysis. This is because now the p-value is corrected
 #(i.e., adjusted) for multiplicity using the Bonferroni test
+
+summary(thuesen.mc)
+
+'The adjusted p-value for the slope parameter is now 0.064 and thus considerably
+smaller than the 0.096 from the Bonferroni test, although we still have
+not achieved significance. During the course of this book we will discuss even
+more powerful methods, which allow the user to safely claim that the slope is
+signicant at the level alpha = 0:05 after having adjusted for multiplicity.'
+
+
